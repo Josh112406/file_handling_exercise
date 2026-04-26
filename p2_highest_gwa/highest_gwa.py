615215a22bacc01613317eb265d1e91f4927b74a
Josh112406 <quibaljosh@gmail.com>
2026-04-26 16:24:25 +0800

    created function to read file

---
+++ p2_highest_gwa/highest_gwa.py
@@ -0,0 +1,8 @@
+class HighestGWA:
+    def __init__(self, filename: str = "gwa.txt"):
+        self.filename = filename
+        
+    def read_file(self) -> list[str]:
+        with open(self.filename, 'r') as file:
+            content = [line.rstrip("\n") for line in file.readlines()]
+        return content
