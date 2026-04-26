12116b210f03f39b3c08c783323c774ee944efb0
Josh112406 <quibaljosh@gmail.com>
2026-04-26 14:54:26 +0800

    add exception handling for read_file

--- p1_even_odd_extractor/even_odd_extractor.py
+++ p1_even_odd_extractor/even_odd_extractor.py
@@ -3,18 +3,28 @@ class OddEvenExtractor:
     def __init__(self, filename: str = "./numbers.txt"):
         self.filename =  filename
         
-    def read_file(self):
-        with open(self.filename, 'r') as file:
-            numbers = [number.rstrip("\n") for number in file.readlines()]
-            
-        return numbers
+    def read_file(self) -> list[int]:
+        try:
+            with open(self.filename, 'r') as file:
+                numbers = [int(number.rstrip("\n")) for number in file.readlines()]
     
-    def file_exists(self, file: str):
-        pass
+            return numbers
+        except:
+            print("Make sure the file only contains integers.")
+            
+    def file_exists(self, file: str) -> bool:
+        if not os.path.exists(file):
+            return False
+        return True
     
     def write_file(self, filename: str):
         pass
         
     def categorize(self):
-        pass
+        data = self.read_file()
+        odd = []
+        even = []
+        for number in data:
+            if number % 2 == 0:
+                even.append
 OddEvenExtractor().read_file()
